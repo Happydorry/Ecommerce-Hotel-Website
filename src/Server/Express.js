ecbb83d20613c18a8f64fe10d09826a8055c4869
Dorsa Peikani <Dorsapeikani12345@gmail.com>
2024-07-16 20:40:10 -0700

    adding http routes

--- src/Server/Express.js
+++ src/Server/Express.js
@@ -6,6 +6,7 @@ import mongoose from "mongoose";
 import { Form } from "./modules/formModule.js";
 const app = express();
 
+app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).send("Everything good!");
 });
@@ -13,7 +14,7 @@ app.get("/", (req, res) => {
 app.post("/form", async (req, res) => {
   try {
     if (!req.body.fullName || !req.body.email || !req.body.password) {
-      return res.status(404).send("All fields required!");
+      return res.status(500).send("All fields required!");
     }
     const newPerson = {
       fullName: req.body.fullName,
@@ -24,6 +25,37 @@ app.post("/form", async (req, res) => {
     return res.status(201).send(person);
   } catch (err) {
     console.log(err);
+  }
+});
+
+app.get("/form", async (req, res) => {
+  try {
+    const people = await Form.find({});
+    return res.status(200).send(people);
+  } catch (err) {
+    return res.status(400).send(err);
+  }
+});
+
+app.put("/form/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const person = await Form.findByIdAndUpdate(id);
+    res.status(200).send("successfully updated");
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Could not update.");
+  }
+});
+
+app.delete("/form/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const person = await Form.findByIdAndDelete(id);
+    res.status(200).send("Successfully deleted.");
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
   }
 });
 
